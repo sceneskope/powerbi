2c1ac8e90a6bef2cd00099b6f4d58a3d7a5145fb
Nick Randell <nick@nickrandell.com>
2017-04-07 10:37:55 +0100

    Added build scripts

---
+++ build.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding(PositionalBinding=$false)]
+param(
+    [string]$BuildNumber="dev",
+    [bool] $CreatePackages=$true,
+    [bool] $RunTests = $true
+)
+
+Write-Host "BuildNumber: $BuildNumber"
+Write-Host "CreatePackages: $CreatePackages"
+Write-Host "RunTests: $RunTests"
+
+Get-ChildItem -Recurse -Filter bin | Remove-Item -Recurse
+Get-ChildItem -Recurse -Filter obj | Remove-Item -Recurse
+
+
+$semVer = Get-Content (Join-Path $PSScriptRoot "semver.txt")
+$version = "$semVer-$BuildNumber"
+
+Write-Host "Restore"
+dotnet restore "/p:Version=$version"
+
+if ($RunTests) {
+    Write-Host "Running tests"
+    Push-Location "$PSScriptRoot\PowerBIClientTests\"
+    dotnet test "/p:Version=$version"
+    Pop-Location
+}
+
+Write-Host "Building"
+dotnet build -c Release "/p:Version=$version"
+
+
+if ($CreatePackages) {
+    Write-Host "Packing"
+    dotnet pack --no-build -c Release "/p:Version=$version"
+}
+Write-Host "Complete"
+
